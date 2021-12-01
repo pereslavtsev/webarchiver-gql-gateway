059d225ac3b244c72e40df66ce5c10756cb01dbf
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-12-02 00:12:08 +0300

    fix(redis): url from env

--- src/modules/shared/services/redis-config.service.ts
+++ src/modules/shared/services/redis-config.service.ts
@@ -5,20 +5,25 @@ import {
   RedisModuleOptionsFactory,
 } from '@nestjs-modules/ioredis';
 import { Bunyan, RootLogger } from '@eropple/nestjs-bunyan';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RedisConfigService
   extends LoggableProvider
   implements RedisModuleOptionsFactory
 {
-  constructor(@RootLogger() rootLogger: Bunyan) {
+  constructor(
+    @RootLogger() rootLogger: Bunyan,
+    private configService: ConfigService,
+  ) {
     super(rootLogger);
   }
 
   createRedisModuleOptions(): RedisModuleOptions {
+    const url = this.configService.get('REDIS_URL');
     return {
       config: {
-        url: 'redis://localhost:6379',
+        url,
       },
     };
   }
